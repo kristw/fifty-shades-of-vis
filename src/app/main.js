8e828b4bda3995a95e210cae910ac6029623b4eb
Krist Wongsuphasawat <krist.wongz@gmail.com>
2016-01-18 15:17:33 -0800

    Update 2016-01-18T23:17:31.057Z

--- src/app/main.js
+++
@@ -1,5 +0,0 @@
-angular.module('app', ['ngVega', 'app.templates'])
-  .controller('mainCtrl', require('./main-controller.js'))
-  .directive('colorPicker', require('./colorPicker.js'))
-  .directive('colorLegend', require('./colorLegend.js'))
-  .directive('visPreview', require('./visPreview.js'));
